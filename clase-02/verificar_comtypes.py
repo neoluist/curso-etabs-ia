0ce2945a509a1e6434f0a7ed30adb54a552bf1dd
neoluist <neoluist@gmail.com>
2026-07-03 01:44:45 -0500

    Add course materials

---
+++ clase-02/verificar_comtypes.py
@@ -0,0 +1,4 @@
+# Clase 02 - Verificar el puente hacia ETABS
+# Si se imprime el mensaje sin errores, comtypes esta instalado y listo.
+import comtypes.client
+print("comtypes listo: el puente hacia ETABS esta instalado")
